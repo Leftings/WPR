3f1622a023a190f9fc90106e47f4bce88e693b58
Leftings <leftingss@gmail.com>
2024-11-24 17:14:38 +0100

    Comments added

--- backend/src/Data/EnvConfig.cs
+++ backend/src/Data/EnvConfig.cs
@@ -1,7 +1,13 @@
-﻿using dotenv.net;
+﻿using dotenv.net; // library om .env files te laden
 using System;
 
 namespace WPR.Data;
+
+/// <summary>
+/// Leez het .env bestand uit waarin de connectie strings staan naar de database.
+/// Deze staan in een apart bestand buiten de git repo,
+/// zodat mensen van buiten niet met de database kunnen verbinden.
+/// </summary>
 
 public class EnvConfig
 {
@@ -9,17 +15,22 @@ public class EnvConfig
     
     public EnvConfig()
     {
-        DotEnv.Load();
+        DotEnv.Load();  // Gebruikt de dotenv.net library om het bestand te laden
 
         var builder = new ConfigurationBuilder()
-            .AddEnvironmentVariables();
+            .AddEnvironmentVariables(); // Voegt de environment variabelen aan de ConfigurationBuilder
         
-        _configuration = builder.Build();
+        _configuration = builder.Build(); // Bouwt het configuratie object
     }
 
+    /// <summary>
+    /// Haalt de value van de specifieke environement variabelen.
+    /// </summary>
+    /// <param name="key">Ophalen environment variabele key.</param>
+    /// <returns>De value van de specifieke environment variabele, of null als niet gevonden.</returns>
     public virtual string Get(string key)
     {
-        var value = _configuration[key];
+        var value = _configuration[key]; // Fetch de value van de gegeven key
         if (string.IsNullOrEmpty(value)) 
         {
             Console.WriteLine($"Warning {key} not found in environment");
@@ -27,6 +38,10 @@ public class EnvConfig
         return value;
     }
 
+    /// <summary>
+    /// Check om te kijken of alle environment variabelen geconfigureerd zijn.
+    /// </summary>
+    /// <returns>True als alle keys present zijn, anders False.</returns>
     public virtual bool IsConfigured()
     {
         var requiredKeys = new[] { "DB_SERVER", "DB_DATABASE", "DB_USERNAME", "DB_PASSWORD" };
