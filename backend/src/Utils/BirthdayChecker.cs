048fcdb24c495236dc4d3a3087d8c72b1bdd7f65
Leftings <leftingss@gmail.com>
2024-11-26 10:52:06 +0100

    Birthday checker

---
+++ backend/src/Utils/BirthdayChecker.cs
@@ -0,0 +1,33 @@
+﻿namespace WPR.Utils;
+
+public class BirthdayChecker
+{
+    public static bool IsValidBirthday(DateTime? birthdate)
+    {
+        if (birthdate == null)
+        {
+            return false;
+        }
+
+        var date = birthdate.Value;
+
+        if (date > DateTime.Now)
+        {
+            return false;
+        }
+        
+        var age = DateTime.Now.Year - date.Year;
+
+        if (date > DateTime.Now.AddYears(-age))
+        {
+            age--;
+        }
+
+        if (age < 18 || age > 120)
+        {
+            return false;
+        }
+        
+        return true;
+    }
+}
