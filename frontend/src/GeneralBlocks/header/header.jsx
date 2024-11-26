bbfc866798085ddb10c21d6f61ef6e09eb78a55a
AfSPhQ36i2Mxfq7 <langelaankoen@gmail.com>
2024-11-26 12:15:24 +0100

    sale page frontend mostly working correctly and terms and conditions page working correctly

--- frontend/src/GeneralBlocks/header/header.jsx
+++ frontend/src/GeneralBlocks/header/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import TermsAndConditions from "../../GeneralSalePage/GeneralSalePage.jsx";
 import './header.css'; 
 
 function GeneralHeader() {
@@ -12,7 +13,7 @@ function GeneralHeader() {
                 <nav id="right">
                     <ul className="nav-links">
                         <li><Link to="/#">Logout</Link></li>
-                        <li><Link to="/cars">Zoek Auto's</Link></li>
+                        <li><Link to="/GeneralSalePage">Zoek Auto's</Link></li>
                         <li><Link to="/about">Over ons</Link></li>
                         <li><Link to="/contact">Contact</Link></li>
                     </ul>
