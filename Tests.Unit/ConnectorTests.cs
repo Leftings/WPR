925f24a8281b6e211373b29804341b73f43c4a0b
Leftings <leftingss@gmail.com>
2024-11-26 10:11:17 +0100

    Mocking connection, no real credentials.

--- Tests.Unit/ConnectorTests.cs
+++ Tests.Unit/ConnectorTests.cs
@@ -1,56 +1,65 @@
-namespace Tests.Unit;
-
-using System;
-using System.Data;
-using Xunit;
-using Moq;
-using WPR.Data;
-using WPR.Database;
-using MySql.Data.MySqlClient;
-
-public class ConnectorTests
+namespace Tests.Unit
 {
-    [Fact]
-    public void DatabaseNotConffigured()
+    using System;
+    using System.Data;
+    using Xunit;
+    using Moq;
+    using WPR.Data;
+    using WPR.Database;
+    using MySql.Data.MySqlClient;
+
+    public class ConnectorTests
     {
-        var envConfig = new Mock<EnvConfig>();
-        envConfig.Setup(x => x.IsConfigured()).Returns(false);
-        var connector = new Connector(envConfig.Object);
+        [Fact]
+        public void DatabaseNotConfigured_ThrowsException()
+        {
+            var envConfig = new Mock<EnvConfig>();
+            envConfig.Setup(x => x.IsConfigured()).Returns(false);
 
-        Assert.Throws<InvalidOperationException>(() => connector.CreateDbConnection());
-    }
+            var connector = new Connector(envConfig.Object);
 
-    [Fact]
-    public void DatabaseConnectionConffigured()
-    {
-        var envConfig = new Mock<EnvConfig>();
-        envConfig.Setup(x => x.IsConfigured()).Returns(true);
-        envConfig.Setup(x => x.Get("DB_SERVER")).Returns("95.99.30.110");
-        envConfig.Setup(x => x.Get("DB_DATABASE")).Returns("WPR");
-        envConfig.Setup(x => x.Get("DB_USERNAME")).Returns("root");
-        envConfig.Setup(x => x.Get("DB_PASSWORD")).Returns("WPR");
+            var exception = Assert.Throws<InvalidOperationException>(() => connector.CreateDbConnection());
+            Assert.Equal("Not configured", exception.Message);
+        }
 
-        var connector = new Connector(envConfig.Object);
+        [Fact]
+        public void DatabaseConnectionConfigured_ReturnsOpenConnection()
+        {
+            var envConfig = new Mock<EnvConfig>();
+            envConfig.Setup(x => x.IsConfigured()).Returns(true);
+            envConfig.Setup(x => x.Get("DB_SERVER")).Returns("fake_server");
+            envConfig.Setup(x => x.Get("DB_DATABASE")).Returns("fake_database");
+            envConfig.Setup(x => x.Get("DB_USERNAME")).Returns("fake_user");
+            envConfig.Setup(x => x.Get("DB_PASSWORD")).Returns("fake_password");
 
-        using var connection = connector.CreateDbConnection();
+            var mockConnection = new Mock<IDbConnection>();
+            mockConnection.Setup(conn => conn.Open()).Callback(() => { });
+            mockConnection.Setup(conn => conn.State).Returns(ConnectionState.Open);
 
-        Assert.NotNull(connection);
-        Assert.Equal(ConnectionState.Open, connection.State);
-    }
+            var connector = new Connector(envConfig.Object);
+            connector = new Mock<Connector>(envConfig.Object) { CallBase = true }.Object;
+            Mock.Get(connector).Setup(c => c.CreateDbConnection()).Returns(mockConnection.Object);
 
-    [Fact]
-    public void DatabaseConnectionWrongConffigured()
-    {
-        var envConfig = new Mock<EnvConfig>();
-        envConfig.Setup(x => x.IsConfigured()).Returns(true);
-        envConfig.Setup(x => x.Get("DB_SERVER")).Returns("95.99.30.110");
-        envConfig.Setup(x => x.Get("DB_DATABASE")).Returns("WPR");
-        envConfig.Setup(x => x.Get("DB_USERNAME")).Returns("incorrect_user"); 
-        envConfig.Setup(x => x.Get("DB_PASSWORD")).Returns("WPR");
+            var createdConnection = connector.CreateDbConnection();
 
-        var connector = new Connector(envConfig.Object);
+            Assert.NotNull(createdConnection);
+            Assert.Equal(ConnectionState.Open, createdConnection.State);
+        }
 
-        var exception = Assert.Throws<MySqlException>(() => connector.CreateDbConnection());
-        Assert.Contains("Access denied for user 'incorrect_user'@", exception.Message);
+        [Fact]
+        public void DatabaseConnectionWrongConfigured_ThrowsMySqlException()
+        {
+            var envConfig = new Mock<EnvConfig>();
+            envConfig.Setup(x => x.IsConfigured()).Returns(true);
+            envConfig.Setup(x => x.Get("DB_SERVER")).Returns("fake_server");
+            envConfig.Setup(x => x.Get("DB_DATABASE")).Returns("fake_database");
+            envConfig.Setup(x => x.Get("DB_USERNAME")).Returns("fake_user");
+            envConfig.Setup(x => x.Get("DB_PASSWORD")).Returns("wrong_password");
+
+            var connector = new Connector(envConfig.Object);
+
+            var exception = Assert.Throws<MySqlException>(() => connector.CreateDbConnection());
+            Assert.Contains("Unable to connect", exception.Message);
+        }
     }
 }
