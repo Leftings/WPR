b5ba0e4dde7cdea5adff78419ff26e99936bb5e4
Jarno Vis <84909095+Jarnovis@users.noreply.github.com>
2024-12-01 16:39:24 +0100

    SessionHandler clean up

--- backend/src/Controllers/Cookie/SessionHandler.cs
+++ backend/src/Controllers/Cookie/SessionHandler.cs
@@ -1,6 +1,6 @@
 namespace WPR.Controllers.Cookie;
 
-public class SessionHandler : IDisposable
+public class SessionHandler
 {
     public void CreateCookie(IResponseCookies responseCookies, string cookieName, string cookieValue)
     {
@@ -9,19 +9,5 @@ public class SessionHandler : IDisposable
             HttpOnly = true, // Cookies zijn alleen toegankelijk voor de server
             Expires = DateTimeOffset.Now.AddMinutes(30) 
         });
-    }
-
-    public void CreateInvalidCookie(IResponseCookies responseCookies, string cookieName)
-    {
-        responseCookies.Append(cookieName, null, new CookieOptions
-        {
-            HttpOnly = true,
-            Expires = DateTimeOffset.Now.AddMinutes(-1)
-        });
-    }
-
-    public void Dispose()
-    {
-        throw new NotImplementedException();
     }
 }
