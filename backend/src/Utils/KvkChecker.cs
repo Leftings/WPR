4d00073641f372432f03569fc5c39faf5695583c
Leftings <leftingss@gmail.com>
2024-12-01 19:25:14 +0100

    Update

---
+++ backend/src/Utils/KvkChecker.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WPR.Utils;
+
+public class KvkChecker
+{
+    public static bool IsValidKvkNumber(int? kvkNumber)
+    {
+        return kvkNumber.HasValue && kvkNumber.Value.ToString().Length == 8;
+    }
+}
