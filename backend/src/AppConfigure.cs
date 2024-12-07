756c4011dc6e7f84a1a233cf0d274fcd1cc18351
Your Name <jd.vis@hotmail.nl>
2024-12-07 17:31:26 +0100

    update

--- backend/src/AppConfigure.cs
+++ backend/src/AppConfigure.cs
@@ -23,161 +23,97 @@ public class AppConfigure
     public static void InitDatabase(IServiceProvider services)
     {
         var dbConnector = services.GetRequiredService<Connector>();
-        
         try
         {
             using (var connection = dbConnector.CreateDbConnection())
             {
                 Console.WriteLine("Database connection established");
             }
-
         }
         catch (Exception e)
         {
             Console.WriteLine("Failed to establish database connection");
             Console.WriteLine(e.StackTrace);
         }
-        
     }
-    /// <summary>
-    /// Configureer de web applicatie, zoals middleware, services, authentication en CORS settings.
-    /// </summary>
-    /// <param name="args">De command line arguments passed to the application at startup.</param>
-    /// <returns>De geconfigureerde WebApplication instantie.</returns>
+
     public static WebApplication ConfigureApplication(string[] args)
     {
-    var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "development";
+        builder.Configuration.AddEnvironmentVariables();
 
-    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "development";
-    builder.Configuration.AddEnvironmentVariables();
-
-    Console.WriteLine(Environment.GetEnvironmentVariable("ASPNETCORE_URLS"));
-    if (environment == "development")
-    {
-        builder.Configuration.AddJsonFile(".env", optional: true, reloadOnChange: true);
-    }
-    else if (environment == "Production")
-    {
-        builder.Configuration.AddJsonFile(".env.deployment", optional: true, reloadOnChange: true);
-    }
-
-    var cookiePolicyOptions = new CookiePolicyOptions
-    {
-        MinimumSameSitePolicy = SameSiteMode.Strict
-    };
-
-
-    builder.Services.AddCors(options =>
-    {
-        /*options.AddPolicy("AllowLocalhost", policy =>
+        Console.WriteLine(Environment.GetEnvironmentVariable("ASPNETCORE_URLS"));
+        if (environment == "development")
         {
-            policy.WithOrigins("http://localhost:5173", "http://95.99.30.110:8080")  // Development URL
-                .AllowAnyHeader()
-                .AllowCredentials()
-                .AllowAnyMethod();
+            builder.Configuration.AddJsonFile(".env", optional: true, reloadOnChange: true);
+        }
+        else if (environment == "Production")
+        {
+            builder.Configuration.AddJsonFile(".env.deployment", optional: true, reloadOnChange: true);
+        }
+
+        var cookiePolicyOptions = new CookiePolicyOptions
+        {
+            MinimumSameSitePolicy = SameSiteMode.Strict
+        };
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowSpecificOrigins", policy =>
+                policy.WithOrigins("https://carandall.nl", "https://95.99.30.110:8443", "http://localhost:5173")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+            );
         });
 
-        options.AddPolicy("AllowProduction", policy =>
-        {
-            policy.WithOrigins("http://carandall.nl", "https://carandall.nl, http://localhost:5173", "http://95.99.30.110:8080") // Production URL
-                .AllowAnyHeader()
-                .AllowCredentials()
-                .AllowAnyMethod();
-        });
-        */
-
-        options.AddPolicy("AllowSpecificOrigins", policy =>
-            policy.WithOrigins("https://95.99.30.110:8443", "http://localhost:5173, http://www.carandall.nl:8080")
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials()
-        );
-    });
-
-    var urls = Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? "http://0.0.0.0:80"; // Default to port 80 if not set
-
-    builder.WebHost.ConfigureKestrel(options =>
-    {
-        /*Uri uri;
-        try
-        {
-            uri = new Uri("http://0.0.0.0:5000");
-        }
-        catch (UriFormatException ex)
-        {
-            Console.WriteLine($"Error parsing the URI: {ex.Message}");
-            uri = new Uri("http://0.0.0.0:5000"); 
-        }
-
-
-        if (uri.Host == "0.0.0.0" || uri.Host == "localhost")
-        {
-            options.Listen(IPAddress.Any, 5000);
-        }
-        else
+        var urls = Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? "http://0.0.0.0:5000";  // Default to port 5000
+        builder.WebHost.ConfigureKestrel(options =>
         {
             try
             {
-                options.Listen(IPAddress.Parse(uri.Host), uri.Port); 
+                Uri uri = new Uri(urls);
+                options.Listen(IPAddress.Parse(uri.Host), uri.Port);
             }
-            catch (FormatException ex)
+            catch (UriFormatException ex)
             {
-                Console.WriteLine($"Invalid IP address specified: {uri.Host}. Using default binding to all IPs.");
-                uri = new Uri("http://0.0.0.0:80");
-                options.Listen(IPAddress.Any, uri.Port);  
+                Console.WriteLine($"Invalid URL format: {urls}. Using default binding to all IPs.");
+                options.Listen(IPAddress.Any, 5001); // Default fallback to 5001
             }
-        }
-        */
-
-        try
-        {
-            Uri uri = new Uri(urls);
-            options.Listen(IPAddress.Parse(uri.Host), uri.Port);
-        }
-        catch (UriFormatException ex)
-        {
-            Console.WriteLine($"Invalid URL format: {urls}. Using default binding to all IPs.");
-            options.Listen(IPAddress.Any, 5001); 
-        }
-    });
-
-
-    // Register services for Dependency Injection
-    builder.Services.AddSingleton<EnvConfig>(); // Singleton for environment configuration
-    builder.Services.AddTransient<Connector>(); // Transient for database connection.
-    builder.Services.AddScoped<IUserRepository, UserRepository>(); // Scoped for user repository
-    builder.Services.AddScoped<SessionHandler>(); // Scoped session handler
-    builder.Services.AddScoped<Crypt>();
-    builder.Services.AddScoped<Hashing.Hash>();
-
-    // Configure authentication with cookie-based authentication schema.
-    builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-        .AddCookie(options =>
-        {
-            options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-            options.SlidingExpiration = true;
-            options.AccessDeniedPath = "/Forbidden/";
         });
 
-    builder.Services.AddControllers()
-        .AddJsonOptions(options =>
+        builder.Services.AddSingleton<EnvConfig>(); // Singleton for environment configuration
+        builder.Services.AddTransient<Connector>(); // Transient for database connection
+        builder.Services.AddScoped<IUserRepository, UserRepository>(); // Scoped for user repository
+        builder.Services.AddScoped<SessionHandler>(); // Scoped session handler
+        builder.Services.AddScoped<Crypt>();
+        builder.Services.AddScoped<Hashing.Hash>();
+
+        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                options.SlidingExpiration = true;
+                options.AccessDeniedPath = "/Forbidden/";
+            });
+
+        builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            });
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsProduction())
         {
-            options.JsonSerializerOptions.ReferenceHandler =
-                ReferenceHandler.IgnoreCycles;
-        });
+            app.UseCors("AllowSpecificOrigins");
+        }
 
-    // Services for Swagger API
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    var app = builder.Build();
-
-    if (app.Environment.IsProduction())
-    {
-        app.UseCors("AllowProduction");
-    }
-    else
-    {
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -187,22 +123,16 @@ public class AppConfigure
                 c.RoutePrefix = string.Empty;
             });
         }
-        app.UseCors("AllowLocalhost");
+
+        app.UseCors("AllowSpecificOrigins");
+        app.UseHttpsRedirection();
+        app.MapControllers();
+        app.UseAuthorization();
+        app.UseAuthentication();
+        app.UseCookiePolicy(cookiePolicyOptions);
+
+        app.MapDefaultControllerRoute();
+
+        return app;
     }
-
-    app.UseCors("AllowSpecificOrigins");
-    app.UseHttpsRedirection();
-    app.MapControllers();
-    app.UseAuthorization();
-    app.UseAuthentication();
-    app.UseCookiePolicy(cookiePolicyOptions);
-
-    //app.MapRazorPages();
-    app.MapDefaultControllerRoute();
-
-    return app;
-    }
-
-
-
 }
