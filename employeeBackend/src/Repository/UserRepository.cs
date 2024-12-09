06245071241a97b0afb91e25ac240ef78bf24f58
Your Name <jd.vis@hotmail.nl>
2024-12-09 17:36:28 +0100

    2 projects 1 soltion commit

--- employeeBackend/src/Repository/UserRepository.cs
+++ employeeBackend/src/Repository/UserRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace WPR.Repository;
+﻿using MySql.Data.MySqlClient;
+
+namespace Employee.Repository;
 
 /// <summary>
 /// De UserRepository class geeft de methodes voor de interactie met de database voor gebruiker gerelateerde operaties.
@@ -14,14 +16,32 @@ public class UserRepository : IUserRepository
     }
     
     // vehicleBlob is het pad naar de afbeelding
-    public Task<bool> AddVehicleAsync(int yop, string brand, string type, string licensPlate, string color, string sort, double price, string description, string vehicleBlob)
+    public async Task<(bool status, string message)> AddVehicleAsync(int yop, string brand, string type, string licensPlate, string color, string sort, double price, string description, string vehicleBlob)
     {
-        string query = "INSERT INTO Vehicle (YoP, Brand, Type, LicensPlate, Color, Sort, Price, Description, Vehicleblob) AS (@Y, @B, @T, @L, @C, @S, @P, @D, @V)";
-
-        using (var connection = _connector.CreateDbConnection())
-        using (var command = new MySqlCommand(query, (MySqlConnection)connection))
+        try
         {
-            
+            string query = "INSERT INTO Vehicle (YoP, Brand, Type, LicensPlate, Color, Sort, Price, Description, Vehicleblob) AS (@Y, @B, @T, @L, @C, @S, @P, @D, @V)";
+
+            using (var connection = _connector.CreateDbConnection())
+            using (var command = new MySqlCommand(query, (MySqlConnection)connection))
+            {
+                command.Parameters.AddWithValue("@Y", yop);
+                command.Parameters.AddWithValue("@B", brand);
+                command.Parameters.AddWithValue("@T", type);
+                command.Parameters.AddWithValue("@L", licensPlate);
+                command.Parameters.AddWithValue("@C", color);
+                command.Parameters.AddWithValue("@S", sort);
+                command.Parameters.AddWithValue("@P", price);
+                command.Parameters.AddWithValue("@D", description);
+                command.Parameters.AddWithValue("@V", vehicleBlob);
+
+                if (await command.ExecuteNonQueryAsync() > 0)
+                {
+                    return (true, "Vehicle inserted");
+                }
+                return (false, "Something went wrong while inserting the vehicle");
+            }
         }
+        catch ()
     }
 }
