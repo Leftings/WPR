6ae49891a7ff102aabbfe94ba6e4966f3dab0bca
Jarno Vis <84909095+Jarnovis@users.noreply.github.com>
2024-12-09 13:53:23 +0100

    back

--- frontend/src/home/home.jsx
+++ frontend/src/home/home.jsx
@@ -5,7 +5,7 @@ import GeneralFooter from "../GeneralBlocks/footer/footer.jsx";
 import './home.css';
 
 // Access the environment variable
-const BACKEND_URL = import.meta.env.VITE_REACT_APP_BACKEND_URL ?? 'http://127.0.0.1:5000';
+const BACKEND_URL = import.meta.env.VITE_REACT_APP_BACKEND_URL ?? 'http://localhost:5165';
 //const BACKEND_URL = "http://95.99.30.110:5000";
 
 console.log('VITE_REACT_APP_BACKEND_URL: ', import.meta.env.VITE_REACT_APP_BACKEND_URL);
