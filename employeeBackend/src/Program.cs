087bcc81c9cfd5a20ec7f368839c569863f493d4
Jarno Vis <84909095+Jarnovis@users.noreply.github.com>
2024-12-10 15:25:47 +0100

    Auto merge by GitHub Actions
    Tests passed successfully.

---
+++ employeeBackend/src/Program.cs
@@ -0,0 +1,17 @@
+namespace Employee;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        var app = AppConfigure.ConfigureApplication(args);
+
+        using (var scope = app.Services.CreateScope())
+        {
+            AppConfigure.InitDatabase(scope.ServiceProvider);
+        }
+
+        app.Run();
+    }
+}
