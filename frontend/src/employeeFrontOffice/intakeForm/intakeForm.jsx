a64e4c0df18bac72034e1f72ef1849d1841e9e1d
ssyhn <saadzk@outlook.com>
2025-01-22 18:07:32 +0100

    Intake exclusief posten van van Intake werkt

--- frontend/src/employeeFrontOffice/intakeForm/intakeForm.jsx
+++ frontend/src/employeeFrontOffice/intakeForm/intakeForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { NumberCheck } from '../../utils/numberFieldChecker.js'
 import '../../index.css';
 import GeneralHeader from '../../GeneralBlocks/header/header';
 import GeneralFooter from '../../GeneralBlocks/footer/footer';
@@ -12,11 +13,51 @@ function IntakeForm() {
     const [damagePresent, setDamagePresent] = useState(false);
     const [isValidContract, setIsValidContract] = useState(false);
     const [contractNumber, setContactNumber] = useState(null);
-    const [damageExplanation, setDamageExplanation] = useState(null);
+    const [damageExplanation, setDamageExplanation] = useState("No damage present.");
     const [endDate, setEndDate] = useState(null);
+    const [vehicleName, setVehicleName] = useState(null);
+    const [contract, setContract] = useState(null);
+    const [orderId, setOrderId] = useState(null);
+    const currDate = new Date();
 
     const handleDamageCheck = (e) => {
         setDamagePresent(e.target.checked);
+    };
+    
+    const fetchContract = async (orderId) => {
+        try {
+            const response = await fetch(`${BACKEND_URL}/api/viewRentalData/GetFullReviewData?id=${orderId}`);
+            
+            if (!response.ok) {
+                throw new Error ("Contract vinden mislukt");
+            }
+            const data = await response.json();
+            
+            const contractData = data.message;
+            
+            setContract(contractData);
+            
+            if (contractData.Vehicle) {
+                setVehicleName(contractData.Vehicle);
+            } else {
+                setVehicleName("Voertuig naam kan niet worden gevonden...")
+            }
+            
+            setError("");
+        } catch (err) {
+            setError(err.message);
+            setContract(null);
+            setVehicleName("");
+        }
+    }
+    
+    const handleSearchClick = () => {
+        
+        if (orderId) {
+            fetchContract(orderId);
+        } else {
+            setError("Please enter a valid Order ID");
+        }
     };
 
     useEffect(() => {
@@ -39,43 +80,76 @@ function IntakeForm() {
                 navigate('/');
             })
     }, [navigate]);
+
+    useEffect(() => {
+        if (contract && (currDate <= new Date(contract.EndDate) && contract.AccountType === 'Private')) {
+            setEndDate(contract.EndDate);
+        }
+        if (contract && (currDate > new Date(contract.EndDate) || contract.AccountType === 'Business')) {
+            setEndDate(null);
+        }
+    }, [contract, currDate]);
+    
     return (
         <>
             <GeneralHeader>
             </GeneralHeader>
 
-            <div className='body'>
-                <h1 className='title-text'>Innameformulier</h1>
+            <div className="body">
+                <h1 className="title-text">Innameformulier</h1>
 
-                <div className='intakeFormFormat'>
-                    <label htmlFor='contract'>Contract</label>
-                    <div className='contractFind'>
-                        <input id='contract'/>
-                        <button id='contract' className='contractFindButton'><i
-                            className="fa-solid fa-magnifying-glass"></i></button>
-                    </div>
-                    <label htmlFor='vehicleName'>Voertuig: N/A</label>
-                    <div className='checkbox-item'>
-                        <label htmlFor='damageCheck'>Schade aanwezig:</label>
-                        <input
-                            type='checkbox'
-                            id='damageCheck'
-                            checked={damagePresent}
-                            onChange={handleDamageCheck}
+                <div className="intakeFormFormat">
+                    <label htmlFor="contract">Contract</label>
+                    <div className="contractFind">
+                        <input 
+                            id="contract"
+                            value={orderId}
+                            onChange={(e) => setOrderId(NumberCheck(e.target.value))}
+                            placeholder="Order ID van contract..."
                         />
+                        <button 
+                            id="contract" 
+                            className="contractFindButton"
+                            onClick={handleSearchClick}
+                        >
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </button>
                     </div>
-
-                    {damagePresent && (
+                    {contract && (
                         <>
-                            <label htmlFor="damageExplanation">Toelichting:</label>
-                            <textarea className="resizable-textarea" id="damageExplanation" placeholder="Licht schade toe..."></textarea>
+                            <label
+                                htmlFor="vehicleName">Voertuig: {contract.Brand} {contract.Type} ({contract.LicensePlate})</label>
+                            <div className="checkbox-item">
+                                <label htmlFor="damageCheck">Schade aanwezig:</label>
+                                <input
+                                    type="checkbox"
+                                    id="damageCheck"
+                                    checked={damagePresent}
+                                    onChange={handleDamageCheck}
+                                />
+                            </div>
+
+                            {damagePresent && (
+                                <>
+                                    <label htmlFor="damageExplanation">Toelichting:</label>
+                                    <textarea
+                                        className="resizable-textarea"
+                                        id="damageExplanation"
+                                        placeholder="Licht schade toe..."
+                                        onChange={(e) => setDamageExplanation(e.target.value)}
+                                    />
+                                </>
+                            )}
+                            {(currDate > new Date(contract.EndDate) || contract.AccountType === 'Business') && (
+                                <>
+                                    <label htmlFor="endDate">Einddatum</label>
+                                    <input type="date" onChange={(e) => setEndDate(e.target.value)}/>
+                                </>
+                            )}
+
+                            <button className="cta-button">Stuur</button>
                         </>
                     )}
-
-                    <label htmlFor="endDate">Einddatum</label>
-                    <input type="date" />
-
-                    <button className="cta-button">Stuur</button>
                 </div>
             </div>
 
