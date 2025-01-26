132d02af908c3aabc86f25d9c19d4bb6da2f3e86
Leftings <leftingss@gmail.com>
2025-01-26 17:32:08 +0100

    Changeusersettings backend test

---
+++ backend/src/Controllers/customer/changeSettings/ChangeUserSettingsErrorResponse.cs
@@ -0,0 +1,7 @@
+namespace WPR.Controllers.Customer.ChangeUserSettings;
+
+public class ChangeUserSettingsErrorResponse
+{
+    public bool Status { get; set; }
+    public string Message { get; set; }
+}
