4631f48774914be656b7f8c6bb415e600ddea14c
Leftings <leftingss@gmail.com>
2025-01-26 15:45:25 +0100

    Frontend Test

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,9 +3,10 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   fixturesFolder: "cypress/fixtures",
   e2e: {
-    baseUrl: "http://localhost:5173",  // Ensure this matches your app's URL
+    baseUrl: "http://localhost:5173",
+    viewportWidth: 1280,
+    viewportHeight: 1024,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
--- frontend/src/utils/errorHandler.jsx
+++ frontend/src/utils/errorHandler.jsx
@@ -1,16 +1,14 @@
 ﻿export const getErrorMessage = (error, signUpType) => {
     const errorMessages = {
-        'Invalid email format' : 'The email format is invalid',
-        'Email already exists' : 'The email is already in use. Please use a different email.',
-        'Invalid phone number' : 'The phone number is invalid.',
-        'Invalid birthday format' : 'The birthday is invalid.',
-        'KVK number must be 8 digits' : 'KVK number must be 8 digits',
-        'KVK number is not a valid KVK number' : 'KVK number is not a valid KVK number',
-        'Password must be at least 8 characters.' : 'Password must be at least 8 characters.',
-        'Password must contain at least one number.' : 'Password must contain at least one number.',
-        'Password must contain at least one upper case letter.' : 'Password must contain at least one upper case letter.',
-        'Password must contain at least one lower case letter.' : 'Password must contain at least one lower case letter.',
-        'Password must contain at least one symbol' : 'Password must contain at least one symbol'
+        'Invalid email format' : 'Verkeerd email format.',
+        'Email already exists' : 'Deze email is al in gebruik.',
+        'Invalid phone number' : 'Het telefoonnummer is niet correct, moet beginnen met: 06.',
+        'Invalid birthday format' : 'Geboortedatum is incorrect',
+        'Password must be at least 8 characters.' : 'Wachtwoord moet 8 karakter zijn',
+        'Password must contain at least one number.' : 'Wachtwoord moet een nummer bevatten',
+        'Password must contain at least one upper case letter.' : 'Wachtwoord moet een hoofdletter bevatten',
+        'Password must contain at least one lower case letter.' : 'Wachtwoord moet een kleine leter bevatten',
+        'Password must contain at least one symbol' : 'Wachtwoord moet een symbool bevatten'
         
     };
     return errorMessages[error.message] || `There was an error during making a ${signUpType} account: ${error.message}`;
